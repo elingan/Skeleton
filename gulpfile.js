f883c83b746fcf5ccc84ce5217d495f3b9e6821f
Stuart George <stuart.bigmassa@gmail.com>
2016-10-25 21:34:39 +0100

    better css min plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var $ = require('gulp-load-plugins')();
 var gulp = require('gulp');
-var rename = require("gulp-rename");
+var prefixer = require('autoprefixer');
 var runSequence = require('run-sequence');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
 
 var config = {
     publicDir: './dist',
@@ -13,20 +11,16 @@ var config = {
 gulp.task('scss', function() {
     return gulp.src(config.scssDir + '/skeleton.scss')
     .pipe($.sourcemaps.init())
-    .pipe(sass({}))
-    .pipe($.postcss([
-        require('autoprefixer')({browsers: ['last 1 version']})
-    ]))
+    .pipe($.sass({}))
+    .pipe($.postcss([prefixer({browsers: ['last 1 version']})]))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
 gulp.task('min', function () {
   return gulp.src(config.publicDir + '/css/skeleton.css')
-    .pipe($.csso())
-    .pipe($.rename({
-        suffix: '.min'
-    }))
+    .pipe($.cssmin())
+    .pipe($.rename({suffix: '.min'}))
     .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
@@ -35,5 +29,10 @@ gulp.task('default', function(cb) {
 });
 
 gulp.task('watch', ['default'], function (){
-    gulp.watch([config.scssDir + '/*.scss', config.scssDir + '/**/*.scss'], ['default']);
+    gulp.watch(
+        [
+            config.scssDir + '/*.scss',
+            config.scssDir + '/**/*.scss'
+        ],
+        ['default']);
 });
